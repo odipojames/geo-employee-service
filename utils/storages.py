c5c0fe69ad31e2e8bacfb929811b8b5ea9674849
odipojames <odipojames12@gmail.com>
2024-07-04 12:50:26 +0300

    overwrite storage

--- utils/storages.py
+++ utils/storages.py
@@ -1,6 +1,6 @@
-from django.core.files.storage import FileSystemStorage
+from cloudinary_storage.storage import MediaCloudinaryStorage
 
-class OverwriteStorage(FileSystemStorage):
+class OverwriteStorage(MediaCloudinaryStorage):
     def get_available_name(self, name, max_length=None):
         self.delete(name)
         return name
