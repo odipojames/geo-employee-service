0d5cc875d06043336b138e4cfad9236edbdf8a00
odipojames <odipojames12@gmail.com>
2024-07-05 10:59:12 +0300

    add general paginaation

--- employee/settings.py
+++ employee/settings.py
@@ -185,6 +185,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTTokenUserAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 5
 }
 
 
