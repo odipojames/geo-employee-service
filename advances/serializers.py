bc1fd7b8e50004ed7888630c78a5de88dbf7514b
odipojames <odipojames12@gmail.com>
2024-07-11 12:04:55 +0300

    validte date

--- advances/serializers.py
+++ advances/serializers.py
@@ -3,13 +3,12 @@ from .models import Advance
 from employees.models import Employee
 from django.utils import timezone
 
-
 class EmployeeMSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
         fields = [
             'id', 'first_name', 'second_name', 'last_name', 'email',
-            'phone', 'id_number', 'department', 'team','age', 'county',
+            'phone', 'id_number', 'department', 'team', 'age', 'county',
             'sub_county', 'salary'
         ]
 
@@ -27,14 +26,27 @@ class AdvanceSerializer(serializers.ModelSerializer):
         if request:
             employee = Employee.objects.get(email=request.user.email)
         
+            # Check if advance amount is greater than the salary
             amount = data.get('amount')
             if amount is not None and amount > employee.salary:
                 raise serializers.ValidationError({"amount": "Advance amount cannot be greater than the salary"})
             
+            # Check if the date is in the past
             date = data.get('date')
             if date and date < timezone.now().date():
                 raise serializers.ValidationError({"date": "The date cannot be in the past."})
+
+            # Check if the employee has already made an advance request in the same month and year
+            current_month = date.month
+            current_year = date.year
+
+            existing_advance = Advance.objects.filter(
+                employee=employee,
+                date__year=current_year,
+                date__month=current_month
+            ).exists()
+            
+            if existing_advance:
+                raise serializers.ValidationError({"detail": "You can only make one advance request per month within the same year."})
         
         return data
-    
-    
